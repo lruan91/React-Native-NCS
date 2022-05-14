126b1192a3d189e7518779e7c3ec3c601008c9db
Lynnea <lruan.dev@gmail.com>
2022-05-14 10:57:42 -0400

    Week 1 Task 1: Creating AboutComponent and ContactComponent and updating MainComponent to import the 2 newly created components

---
+++ components/AboutComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+//Week 1 Task 1
+class About extends Component {
+
+  static navigationOptions = {
+    title: 'About Us'
+  }
+  render() {
+    return(
+      <ScrollView></ScrollView>
+    )
+  }
+}
+
+export default About;
