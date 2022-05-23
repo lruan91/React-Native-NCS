966fc7bd583269c5a141f3e9270b68db47c077db
Lynnea <lruan.dev@gmail.com>
2022-05-23 17:05:06 -0400

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -31,6 +31,24 @@ export const addComments = comments => ({
   payload: comments
 });
 
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+  const newComment = {
+    campsiteId,
+    rating,
+    author,
+    text
+  };
+  newComment.date = new Date().toISOString();
+  setTimeout(() => {
+    dispatch(addComment(newComment));
+  }, 2000);
+}
+
+export const addComment = (comment) => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: comment
+});
+
 export const fetchCampsites = () => dispatch => {
   dispatch(campsitesLoading());
 
@@ -150,21 +168,8 @@ export const addFavorite = campsiteId => ({
   type: ActionTypes.ADD_FAVORITE,
   payload: campsiteId
 });
-
-export const postComment = (campsiteId, rating, author, text) => dispatch => {
-  const newComment = {
-    campsiteId,
-    rating,
-    author,
-    text
-  };
-  newComment.date = new Date().toISOString();
-  setTimeout(() => {
-    dispatch(addComment(newComment));
-  }, 2000);
-}
-
-export const addComment = (comment) => ({
-  type: ActionTypes.ADD_COMMENT,
-  payload: comment
+// non thunked actioncreator
+export const deleteFavorite = campsiteId => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId
 });
